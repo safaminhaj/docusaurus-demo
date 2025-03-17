24217b76926d2a0df661b2144b2a69e298d898da
safaminhaj <safaminhaj11@gmail.com>
2025-03-17 16:34:17 +0530

    fixed edit url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -54,7 +54,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             // "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-            "https://github.com/safaminhaj/docusaurus-demo/main/",
+            "https://github.com/safaminhaj/docusaurus-demo/edit/main/",
         },
         blog: {
           showReadingTime: true,
