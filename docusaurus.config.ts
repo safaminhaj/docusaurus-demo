6a42c2bc05cd500f74716d53107423e9d25868dc
safaminhaj <safaminhaj11@gmail.com>
2025-03-20 21:34:11 +0530

    removed footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -120,51 +120,51 @@ const config: Config = {
         },
       ],
     },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Tutorial",
-              to: "/docs/intro",
-            },
-          ],
-        },
-        // {
-        //   title: "Community",
-        //   items: [
-        //     {
-        //       label: "Stack Overflow",
-        //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
-        //     },
-        //     {
-        //       label: "Discord",
-        //       href: "https://discordapp.com/invite/docusaurus",
-        //     },
-        //     {
-        //       label: "X",
-        //       href: "https://x.com/docusaurus",
-        //     },
-        //   ],
-        // },
-        // {
-        //   title: "More",
-        //   items: [
-        //     {
-        //       label: "Blog",
-        //       to: "/blog",
-        //     },
-        //     {
-        //       label: "GitHub",
-        //       href: "https://github.com/facebook/docusaurus",
-        //     },
-        //   ],
-        // },
-      ],
-      copyright: `Copyright © Compliance Cloud ${new Date().getFullYear()}`,
-    },
+    // footer: {
+    //   style: "dark",
+    //   links: [
+    //     {
+    //       title: "Docs",
+    //       items: [
+    //         {
+    //           label: "Tutorial",
+    //           to: "/docs/intro",
+    //         },
+    //       ],
+    //     },
+    //     // {
+    //     //   title: "Community",
+    //     //   items: [
+    //     //     {
+    //     //       label: "Stack Overflow",
+    //     //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
+    //     //     },
+    //     //     {
+    //     //       label: "Discord",
+    //     //       href: "https://discordapp.com/invite/docusaurus",
+    //     //     },
+    //     //     {
+    //     //       label: "X",
+    //     //       href: "https://x.com/docusaurus",
+    //     //     },
+    //     //   ],
+    //     // },
+    //     // {
+    //     //   title: "More",
+    //     //   items: [
+    //     //     {
+    //     //       label: "Blog",
+    //     //       to: "/blog",
+    //     //     },
+    //     //     {
+    //     //       label: "GitHub",
+    //     //       href: "https://github.com/facebook/docusaurus",
+    //     //     },
+    //     //   ],
+    //     // },
+    //   ],
+    //   copyright: `Copyright © Compliance Cloud ${new Date().getFullYear()}`,
+    // },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
